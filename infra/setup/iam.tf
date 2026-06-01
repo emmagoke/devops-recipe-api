bcda22538f94b63acfe6b50049bfc02b6685ff2d
Emmanuel Olagoke <emma.goke05@gmail.com>
2026-06-01 17:14:55 +0100

    added a linked role permission to the iam

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -266,7 +266,9 @@ data "aws_iam_policy_document" "iam" {
       "iam:TagPolicy",
       "iam:PassRole",
       # Add this action right here:
-      "iam:CreateServiceLinkedRole"
+      "iam:CreateServiceLinkedRole",
+      # Add this action right here:
+      "iam:DeleteServiceLinkedRole"
     ]
     resources = ["*"]
   }
