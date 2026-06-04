e1824cbf60ed2b73bf814e1b777d05c919540423
Emmanuel Olagoke <emma.goke05@gmail.com>
2026-06-04 14:01:15 +0100

    added efs resource

---
+++ infra/deploy/efs.tf
@@ -0,0 +1,10 @@
+#########################
+# EFS for Media Storage #
+#########################
+
+resource "aws_efs_file_system" "media" {
+  encrypted = true
+  tags = {
+    Name = "${local.prefix}-media"
+  }
+}
